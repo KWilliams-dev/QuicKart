a8c8c72d10cd7c80bccccfd6c556e0c9f0409ed0
amunye <amunye1@ggc.edu>
2022-09-18 13:20:34 -0400

    added database

--- src/model/Item.js
+++
@@ -1,3 +0,0 @@
-// Model the data for a item here in MongoDB.
-// Look into using realm (MongoDB tech) https://www.mongodb.com/docs/realm/sdk/react-native/
-// Write code to write Item objects to database
