27501b58c5e15fa612d6e5eae617d5e63a9ffdc7
Khamilah Nixon <knixon4@ggc.edu>
2022-09-21 18:58:51 -0400

    Create index.js

---
+++ src/model/map/index.js
@@ -0,0 +1,105 @@
+
+const dotenv = require('dotenv');
+dotenv.config();
+const { ApolloServer, gql } = require('apollo-server');
+const { MongoClient, ObjectID } = require('mongodb');
+const { assertValidSDLExtension } = require('graphql/validation/validate');
+
+const typeDefs = gql`
+    type Query {
+        getItem (
+          id:ID!,
+          name:String!,
+        ):Item
+    }
+    
+    type Item{
+      id:ID!,
+      name:String!,
+      aisle:String!,
+      bay:String!,
+      price:Float!,
+      xVal:Int!,
+      yVal:Int!
+    }
+    
+    type StoreMap {
+      id: ID!,
+      description: String,
+      aisle: [Aisle!]!,
+      width: Int!,
+      length: Int!,
+      entrance: [[Int!]!]! 
+    }
+    
+    input MapInput {
+      description: String, 
+      aisle: [Aisle!]!, 
+      width: Int!, 
+      length: Int!
+    }
+
+    type User {
+      id: ID!,
+      shoppingList: [Item!]!,
+      startingLocation: [[Int!]!]! 
+    }
+
+    type Mutation {
+        createItem(name: String!, aisle: String!): Item!
+        
+        createMap(input: MapInput!): StoreMap!
+        getMap(id: ID!): [[Int!]!]!
+    }
+
+`;
+
+
+
+const resolvers = {
+  Query:  {
+
+  },
+  Mutation: {
+    createMap: async (_, { description, width, length }, { db, aisle }) => {
+        if(!aisle) { throw new Error('Aisle does not exist') }
+
+        const newMap = {
+          description, 
+          aisle: [aisle],
+          width,
+          length
+        }
+        
+        const result = await db.collection('Map').insert(newMap);
+
+        return result.ops[0]
+    },
+    getMap: async (_, { id }, { db }) => {
+      return await db.collection('Map').findOne({ _id: ObjectID(id) });
+    }
+  },
+  StoreMap: {
+    id: ({ _id, id }) => id || id,
+  },
+};
+
+const start = async () => {
+  const client = new MongoClient("mongodb+srv://admin:admin@quickkartcluster.o0bsfej.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db("QuickKart");
+ 
+  const server = new ApolloServer({
+      typeDefs,
+      resolvers,
+      context: db,
+      introspection: true
+  }); 
+  
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+      console.log(`🚀  Server ready at ${url + 'quickkart'}`);
+  });
+}
+
+start();
