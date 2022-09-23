39cb699f2900f1da66e659c89dd432e7587efa52
Kyle Williams <KWilliams-dev@users.noreply.github.com>
2022-09-23 13:42:55 -0400

    ShoppingList screen addition

--- src/view/screens/ShoppingList.js
+++ src/view/screens/ShoppingList.js
@@ -1,77 +1,106 @@
 import * as React from 'react';
+import {useState, useEffect}  from 'react';
 import { View, StyleSheet, FlatList, Text as NativeText} from 'react-native';
-import { Searchbar } from 'react-native-paper';
 import { Button, Text } from 'react-native-paper';
+import SearchableDropdown from 'react-native-searchable-dropdown';
 
 const DATA = [
     {
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-      title: 'Banana',
+      id: 1,
+      name: 'Banana',
     },
     {
-      id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-      title: 'Bread',
+      id: 2,
+      name: 'Apple',
     },
     {
-      id: '58694a0f-3da1-471f-bd96-145571e29d72',
-      title: 'Eggs',
+      id: 3,
+      name: 'Doritos',
+    },
+    {
+      id: 4,
+      name: 'Oreos',
+    },
+    {
+      id: 5,
+      name: 'Ice cream',
+    },
+    {
+      id: 6,
+      name: 'Blueberries',
+    },
+    {
+      id: 7,
+      name: 'Bread',
+    },
+    {
+      id: 8,
+      name: 'Oranges',
     },
   ];
 
 export const ShoppingListScreen = () => {
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    useEffect(() => {
+        console.log(selectedItems)
+    },[selectedItems]) 
+
     return (
     <View style={styles.container}>
-        <Text style={styles.text} variant='displayMedium'>Shopping List</Text>
-        <Searchbar style={styles.searchbar} placeholder="Search" />
+        <NativeText style={styles.titleText}>Shopping List</NativeText>
+        
         <View style={styles.flatList}>
-            <FlatList data={DATA}
-            renderItem={({item}) => {
-                return(
-            <NativeText style={styles.item}>{item.title}</NativeText>
-                );
+            <FlatList data={selectedItems}
+            renderItem={({item}) => { return(<NativeText style={styles.item}>{item.name}</NativeText>);
             }}
+            extraData={selectedItems}
             />
         </View>
         <View style={styles.bottomContainer}>
             <Text style={styles.bottomText} variant='titleLarge'>Total Cost:      </Text>
-            <Text style={styles.bottomText} variant='titleLarge'>Grocery Count: {DATA.length}</Text>
+            <Text style={styles.bottomText} variant='titleLarge'>Grocery Count: {selectedItems.length}</Text>
         </View>
         <Button style={styles.bottomButton} buttonColor='blue' mode='contained'><Text style={styles.bottomText} variant='headlineMedium'>START SHOPPING</Text></Button>
-    </View>
+     </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#FFF3F0',
+        backgroundColor: '#F2FFFF',
         alignItems: 'center',
         width: '100%',
-        height: '100%'
+        height: '100%',
+        paddingTop:75,
     },
-    text: {
-        marginTop: 50,
+    titleText: {
+        marginTop: 10,
+        fontSize: 46
     },
     button: {
         height: '100%'
-    },
-    searchbar: {
-        marginTop: 30,
-        width: 325
     },
     flatList: {
         backgroundColor: 'white',
         marginTop: 20,
         width: '85%',
         height: '50%',
-        borderRadius: 15
+        borderRadius: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
     },
     item: {
         paddingLeft: 25,
         paddingTop: 15,
         padding: 10,
-        fontSize: 18,
-        height: 44
+        fontSize: 20,
+        height: 44,
+        color: '#5A5A5A'
     },
     bottomContainer: {
         height: '7%',
@@ -80,13 +109,35 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         width: '85%',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        marginTop: 10,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
     },
     bottomText: {
         color: 'white',
         fontWeight: 'bold'
     },
     bottomButton: {
-        marginTop: 30
+        marginTop: 30,
+        backgroundColor: '#3F7CAC',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.8,
+        shadowRadius: 2,  
+        elevation: 5
+    },
+    dropdown: {
+        width: '85%',
+        marginTop: 30,
+        backgroundColor: 'white',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,  
+        elevation: 5
     }
 });
