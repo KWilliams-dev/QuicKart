b2719e83e785647cf2ed875c8297b9e1cfee4bc3
amunye <amunye1@ggc.edu>
2022-09-25 20:30:58 -0400

    added insertion to db

--- src/model/items/index.js
+++ src/model/items/index.js
@@ -1,15 +1,25 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
 const { ApolloServer, gql } = require('apollo-server');
-const { MongoClient } = require('mongodb');
+const { MongoClient,ObjectID } = require('mongodb');
+
+
+
+
+
+  // type Query {
+    //     getItem (id:ID!,name:String!,):Item
+    // }
+
+    //getItem(id: ID!):Item
 
 const typeDefs = gql`
+
+
     type Query {
-        getItem (
-          id:ID!,
-          name:String!,
-        ):Item
+      getItem (
+        id:ID!,
+      ):Item
     }
     
     type Item{
@@ -22,27 +32,53 @@ const typeDefs = gql`
       yVal:Int!
     }
 
+    #created to create the items in apollographql
     type Mutation {
-        createItem(name: String!, aisle: String!): Item!
-    }
-
+      createItem(name: String!,aisle:String!,bay:String!,price:Float!,xVal:Int!,yVal:Int!): Item!
+      }
+   
 `;
+
+// type Mutation {
+//   createItem(name: String!, aisle: String!): Item!
+// }
+
+
 
 
 
 const resolvers = {
-  Query:  {
-
+Query:  {
+  getItem: async(_, { id }, { db }) => {
+    return await db.collection('Item').findOne({ _id: ObjectID(id) });
   },
-  Mutation: {
-
   },
+  // getItem: async(_,{id},{db}) => {
+  //   return await db.collection('Item').findOne({_id:ObjectID(id)})}
+// },
+
+Mutation: {
+createItem:async(_, {name, aisle, bay, price, xVal, yVal},{db}) => {
+
+const newItem = {name, aisle, bay, price, xVal, yVal, createdAt: new Date().toISOString()
+}
+
+const result = await db.collection('Item').insert(newItem);
+return result.ops[0];
+}
+},
+
+Item:{
+id: ( { _id, id }) => _id || id,
+}
+
 };
+
 
 const start = async () => {
   const client = new MongoClient("mongodb+srv://admin:admin@quickkartcluster.o0bsfej.mongodb.net/test", { useNewUrlParser: true, useUnifiedTopology: true });
   await client.connect();
-  const db = client.db("QuickKart");
+  const db = client.db("quickKart");
  
   const context = {
     db,
---
+++ src/model/items/itemsListData.js
@@ -0,0 +1,236 @@
+const items = [{
+    id: '000',
+    name: 'apple',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '001',
+    name: 'Orange',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '002',
+    name: 'Pears',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '003',
+    name: 'Strawberries',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '004',
+    name: 'BlueBerries',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '005',
+    name: 'Bananas',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '006',
+    name: 'Salmon',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '007',
+    name: 'Tuna',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '008',
+    name: 'Cod',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '009',
+    name: 'SwordFish',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '010',
+    name: 'Ham',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '011',
+    name: 'Turkey',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '012',
+    name: 'Chedder Cheese',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '013',
+    name: 'Pepper Jack Cheese',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+{
+    id: '014',
+    name: 'Greek Yogurt',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '015',
+    name: 'Yogurt',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '016',
+    name: 'White Bread',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '017',
+    name: 'Muffin',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '018',
+    name: 'Donuts',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '019',
+    name: 'Coke',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '020',
+    name: 'Sprite',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '021',
+    name: 'Cheetos',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '022',
+    name: 'Doritos',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '023',
+    name: 'SunChips',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '024',
+    name: 'Cheese Sticks',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  {
+    id: '025',
+    name: 'Cheese Cake',
+    aisle: '1',
+    bay: '2',
+    price: "200",
+    xVal: '0',
+   yVal:'0'
+  },
+  ]
+  module.exports =items
