ccb85ac413d30328bc0e16de1d8974f4aeb02ac7
amunye1 <80010527+amunye1@users.noreply.github.com>
2022-11-02 18:57:05 -0400

    Changes add to the Shopping route page

--- src/view/redux/groceryListAction.js
+++ src/view/redux/groceryListAction.js
@@ -1,8 +1,17 @@
 export const SET_GROCERY_LIST = 'SET_GROCERY_LIST';
+export const Delete_Item = 'Delete_Item';
+export const Add_Item = 'Add_Item';
 
 export const setGroceryList = groceryList => dispatch => {
     dispatch({
         type: SET_GROCERY_LIST,
         payload: groceryList
+    })
+}
+
+export const deleteGroceryList = groceryList =>dispatch=>{
+    dispatch({
+        type:Delete_Item,
+        payload:groceryList
     })
 }
