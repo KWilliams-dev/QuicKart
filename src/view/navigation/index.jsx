d67f728a5bdf633c64c3d66824158da313f67a46
Alan Oliver <alanoli99@gmail.com>
2022-11-06 14:05:21 -0500

    Created the user account to sign in and sign up. trying to implement the UI of it

--- src/view/navigation/index.jsx
+++ src/view/navigation/index.jsx
@@ -5,6 +5,7 @@ import { Welcome } from '../screens/Welcome';
 import { ShoppingListScreen } from '../screens/ShoppingList';
 import { ShoppingRouteScreen } from '../screens/ShoppingRoute';
 import { FinishShoppingScreen } from '../screens/FinishShopping';
+import SignInScreen from '../screens/SignInScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -18,9 +19,10 @@ export default function Navigation() {
 
 function RootNavigator() {
     return(
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator initialRouteName="SignIn">
             <Stack.Screen name="Home" component={Welcome} options={{title: 'Welcome!'}}/>
             <Stack.Screen name="ShoppingList" component={ShoppingListScreen} options={{title: 'Your Grocery List!'}}/>
+            <Stack.Screen name="SignIn" component={SignInScreen} options={{title: 'Please Sign In!'}}/>
             <Stack.Screen name="ShoppingRoute" component={ShoppingRouteScreen} options={{title: 'Fastest Route!'}} />
             <Stack.Screen name="ShoppingFinish" component={FinishShoppingScreen} options={{title: "You're all done!"}}/>
         </Stack.Navigator>
