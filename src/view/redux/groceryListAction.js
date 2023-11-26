863a4c646d69a52f7050062f9489828e149670ef
Rii Holli <103677691+rhollins8199@users.noreply.github.com>
2023-11-25 19:38:54 -0500

    Updated groceryListAction.js
    Added action function to set the count value.

--- src/view/redux/groceryListAction.js
+++ src/view/redux/groceryListAction.js
@@ -1,10 +1,18 @@
-export const SET_GROCERY_LIST = 'SET_GROCERY_LIST';
+export const SET_TOTAL = 'SET_TOTAL';
+export const SET_COUNT = 'SET_COUNT';
 
+// Action creator function to set the total value
+export const setTotal = (total) => {
+  return {
+    type: SET_TOTAL, 
+    payload: total,  
+  };
+};
 
-export const setGroceryList = groceryList => dispatch => {
-    dispatch({
-        type: SET_GROCERY_LIST,
-        payload: groceryList
-    })
-}
-
+// Action creator function to set the count value
+export const setCount = (count) => {
+  return {
+    type: SET_COUNT, 
+    payload: count, 
+  };
+};
