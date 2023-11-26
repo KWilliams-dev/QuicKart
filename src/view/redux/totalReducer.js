19ad2d3060eab25053ebdf9ad8095b0af349751f
Rii Holli <103677691+rhollins8199@users.noreply.github.com>
2023-11-25 19:45:44 -0500

    Updated and added comments to totalReducer.js
    Added case for handling the SET_COUNT action.

--- src/view/redux/totalReducer.js
+++ src/view/redux/totalReducer.js
@@ -1,13 +1,19 @@
 import { SET_TOTAL } from "./totalActions";
+import { SET_COUNT } from "./totalActions";
 
 const initialState = {
-    total: 0
+    total: 0.00, // Initial value for the total property
+    count: 0  // Initial value for the count property
 }
 
 function totalReducer(state = initialState, action) {
     switch(action.type) {
+        // Case for handling the SET_TOTAL action
         case SET_TOTAL:
             return {...state, total: action.payload };
+         // Case for handling the SET_COUNT action
+        case SET_COUNT:
+            return {...state, count: action.payload };
         default: 
             return state
     } 
