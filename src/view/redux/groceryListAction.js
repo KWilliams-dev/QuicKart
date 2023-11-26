f39e5427cf01e144c933306f3db6c0d1219a951d
Rii Holli <103677691+rhollins8199@users.noreply.github.com>
2023-11-25 19:43:31 -0500

    Updated groceryListAction.js

--- src/view/redux/groceryListAction.js
+++ src/view/redux/groceryListAction.js
@@ -1,18 +1,9 @@
-export const SET_TOTAL = 'SET_TOTAL';
-export const SET_COUNT = 'SET_COUNT';
+export const SET_GROCERY_LIST = 'SET_GROCERY_LIST';
 
-// Action creator function to set the total value
-export const setTotal = (total) => {
-  return {
-    type: SET_TOTAL, 
-    payload: total,  
-  };
-};
 
-// Action creator function to set the count value
-export const setCount = (count) => {
-  return {
-    type: SET_COUNT, 
-    payload: count, 
-  };
-};
+export const setGroceryList = groceryList => dispatch => {
+    dispatch({
+        type: SET_GROCERY_LIST,
+        payload: groceryList
+    })
+}
