ab000e5d40cb7a45f6d4cc670a26120a16cbbfb2
Rii Holli <103677691+rhollins8199@users.noreply.github.com>
2023-11-25 19:44:30 -0500

    Updated and added comment to totalActions.js
    Added action function to set the count value.

--- src/view/redux/totalActions.js
+++ src/view/redux/totalActions.js
@@ -1,8 +1,18 @@
-export const SET_TOTAL = 'SET_TOTAL'
+export const SET_TOTAL = 'SET_TOTAL';
+export const SET_COUNT = 'SET_COUNT';
 
-export const setTotal = total => dispatch => {
-    dispatch({
-        type: SET_TOTAL,
-        payload: total
-    })
-}
+// Action creator function to set the total value
+export const setTotal = (total) => {
+  return {
+    type: SET_TOTAL, 
+    payload: total,  
+  };
+};
+
+// Action creator function to set the count value
+export const setCount = (count) => {
+  return {
+    type: SET_COUNT, 
+    payload: count, 
+  };
+};
